f1126268f3f0ace2d3f5a1d2787ef1592da18fde
Abdisemed Abdullahi <abdisemed20@gmail.com>
2023-12-06 14:51:52 +0300

    completed

--- lib/file_io.py
+++ lib/file_io.py
@@ -1,8 +1,27 @@
 def write_file(file_name, file_content):
-    pass
+    file_name = str(file_name)
+
+    file_name += ".txt"
+
+    with open(file_name, mode = 'w', encoding = "utf-8") as file:
+        file.write(file_content)
 
 def append_file(file_name, append_content):
-    pass
+
+    file_name = str(file_name)
+
+    file_name  += ".txt"
+
+    with open(file_name, mode ='a', encoding = "utf-8") as file:
+        file.write(append_content)
 
 def read_file(file_name):
-    pass
+
+    file_name = str(file_name)
+
+    file_name += ".txt"
+
+    with open(file_name, mode = 'r', encoding = "utf-8") as file:
+        content = file.read()
+        print(content)
+        return content
